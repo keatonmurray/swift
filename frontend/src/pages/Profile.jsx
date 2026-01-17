5e329391a11fd2451557ec18962f10f8c23526ca
Keaton Murray <murraykeaton422@gmail.com>
2026-01-16 23:13:17 -0800

    adding padding

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -10,23 +10,23 @@ const Profile = () => {
         </div>
         <div className="profile-nav-menu w-100 mt-4">
             <ul className="list-group">
-                <li className="list-group-item fw-semibold py-4 border-top-0 d-flex align-items-center">
+                <li className="list-group-item fw-semibold py-4 px-3 border-top-0 d-flex align-items-center">
                     <img src="./img/pen.png" alt="Edit" />
-                    Edit Profile
+                    Update Profile
                 </li>
-                <li className="list-group-item fw-semibold py-4 border-top-0 d-flex align-items-center">
+                <li className="list-group-item fw-semibold py-4 px-3 border-top-0 d-flex align-items-center">
                     <img src="./img/invoice.png" alt="Invoices" />
-                    Invoices
+                    Download Invoice
                 </li>
-                <li className="list-group-item fw-semibold py-4 border-top-0 d-flex align-items-center">
+                <li className="list-group-item fw-semibold py-4 px-3 border-top-0 d-flex align-items-center">
                     <img src="./img/payments.png" alt="Payments" />
-                    Payments
+                    Review Payments
                 </li>
-                <li className="list-group-item fw-semibold py-4 border-top-0 d-flex align-items-center">
+                <li className="list-group-item fw-semibold py-4 px-3 border-top-0 d-flex align-items-center">
                     <img src="./img/calculator.png" alt="Accounts" />
-                    Accounts
+                    Manage Accounts
                 </li>
-                <li className="list-group-item fw-semibold py-4 border-top-0 d-flex align-items-center">
+                <li className="list-group-item fw-semibold py-4 px-3 border-top-0 d-flex align-items-center">
                     <img src="./img/logout.png" alt="Logout" />
                     Logout
                 </li>
