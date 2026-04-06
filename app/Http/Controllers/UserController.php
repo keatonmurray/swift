1043b84b5d7f8d7d46b48465608083613cd15627
Keaton Murray <murraykeaton422@gmail.com>
2026-04-05 23:36:11 -0700

    adding missing parameter

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -27,7 +27,7 @@ class UserController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, RapydService $rapyd)
     {
 
         $validator = Validator::make($request->all(), [
