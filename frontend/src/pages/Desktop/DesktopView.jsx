cc4143e78b357f17c1ba601b30cf926f1b6eb9d3
Keaton Murray <murraykeaton422@gmail.com>
2026-05-14 09:06:58 -0700

    replacing logo

--- frontend/src/pages/Desktop/DesktopView.jsx
+++ frontend/src/pages/Desktop/DesktopView.jsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import Dashboard from "./pages/personal/Dashboard";
 
 const DesktopView = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/personal/dashboard" element={<Dashboard />} />
     </Routes>
   )
 }
