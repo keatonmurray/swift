9da2346a52bf6fd5e63bfb5e182c97bfa5443bd2
Keaton Murray <murraykeaton422@gmail.com>
2026-05-14 22:51:55 -0700

    fixing sidebar width and responsiveness

--- frontend/src/pages/Desktop/pages/personal/Dashboard.jsx
+++ frontend/src/pages/Desktop/pages/personal/Dashboard.jsx
@@ -6,14 +6,15 @@ import PersonalDashboardSidebar from "../../components/PersonalDashboardSidebar"
 const Dashboard = () => {
 
     return (
-        <div className="swift desktop-view homepage m-0 p-0">
-            <div className="row m-0">
-                <div className="col-12 col-md-2 d-md-block d-none m-0 p-0 min-vh-100 overflow-auto">
-                    <PersonalDashboardSidebar />
-                </div>
-                <div className="col-12 col-md-10 d-block m-0 p-0">
-                    
-                </div>
+        <div className="swift desktop-view homepage d-flex m-0 p-0 min-vh-100">
+            {/* Sidebar */}
+            <div className="sidebar bg-dark text-white d-none d-md-block">
+                <PersonalDashboardSidebar />
+            </div>
+
+            {/* Main content */}
+            <div className="content flex-grow-1">
+                {/* page content */}
             </div>
         </div>
     )
